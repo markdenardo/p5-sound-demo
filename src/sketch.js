6fe57dda3072027fc1ef764b32c127c127340660
markdenardo <mark.denardo@gmail.com>
2020-09-30 21:19:55 -0400

    canvas

--- src/sketch.js
+++ src/sketch.js
@@ -1,10 +1,12 @@
+import p5 from '../node_modules/p5';
 
 
- function sketch(p) {
+
+function sketch(p) {
     let rotation = 0;
 
     p.setup = function () {
-        p.createCanvas(100, 100);
+        p.createCanvas(500,500);
     };
 
     p.draw = function () {
@@ -16,7 +18,8 @@
         p.ellipse(p.mouseX, p.mouseY, 80, 80);
     };
 
-     
+
+
 };
 
 export default sketch;
