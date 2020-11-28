4935f21f094907691866ead7864578b3b8130673
markdenardo <mark.denardo@gmail.com>
2020-11-27 19:41:19 -0500

    clock

---
+++ src/Clock.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+function Clock(props) {
+  const [date, setDate] = useState(new Date());
+
+ //Replaces componentDidMount and componentWillUnmount
+ useEffect(() => {
+  var timerID = setInterval( () => tick(), 1000 );
+
+  return function cleanup() {
+      clearInterval(timerID);
+    };
+ });
+
+   function tick() {
+    setDate(new Date());
+   }
+
+   return (
+      <div>
+        <h1>Hello, world!</h1>
+           <h2>It is {date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+}
+
+export default Clock
--- src/text.js
+++ src/text.js
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 
 import React from 'react';
+import Clock from './Clock'
 
 const Text = () => {
     return (<div className="Text">
@@ -8,7 +9,7 @@ const Text = () => {
             <div>Instagram</div>
             <div>CV</div>
             <div>Github</div>
-
+            <Clock/>
             </div>
            )
 }
